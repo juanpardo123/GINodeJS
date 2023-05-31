38d5343a739ef9272cf9721d8e595df8d576689a
juanpardo123 <mrstudent.bear@gmail.com>
2023-05-31 18:24:02 -0400

    e

--- Hard/hard.js
+++ Hard/hard.js
@@ -3,10 +3,14 @@
 //npm i express body-parser
 //npm i nodemon
 
-
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+let rawdata = fs.readFileSync('./hard.json');
+let employees = JSON.parse(rawdata);
+
+console.log(typeof employees);
 //creating instance for experess 
 
 const app = express();
@@ -21,11 +25,37 @@ app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
-app.get('', (req, res)=>{
-    res.render('hard')
+
+app.get('/:id', (req, res)=>{
+
+    employees.employees.forEach(element => {
+        let id = parseInt(req.params.id) 
+        if(element.employeeID == id){
+            
+            res.render('hard', 
+            {id: element.employeeID,
+            name: element.name,
+            salary: element.salary,
+            department: element.department
+            })
+        }
+        
+    });
+
+        res.render('errors')
+   
+})
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/employees', (req, res)=>{
+    res.render('employees')
 })
 
 app.listen(port, ()=>{
     console.log(`listening at port: ${port}`);
 })
 
+app.locals.employees = employees;
